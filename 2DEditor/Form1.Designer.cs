3b7ae1880fe68c6cc074ccd0ab2f91d84081a9b2
medmene <38806186+medmene@users.noreply.github.com>
2018-11-27 16:34:30 +0700

    Add files via upload

---
+++ 2DEditor/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace _2DEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pointsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteSelectedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rollbackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.centerPointToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Calibri", 13.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(1256, 671);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Export";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("Calibri", 13.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(1164, 671);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Import";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1320, 638);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pointsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1344, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pointsToolStripMenuItem
+            // 
+            this.pointsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deleteSelectedToolStripMenuItem,
+            this.rollbackToolStripMenuItem,
+            this.clearAllToolStripMenuItem,
+            this.centerPointToolStripMenuItem});
+            this.pointsToolStripMenuItem.Name = "pointsToolStripMenuItem";
+            this.pointsToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.pointsToolStripMenuItem.Text = "Points";
+            // 
+            // deleteSelectedToolStripMenuItem
+            // 
+            this.deleteSelectedToolStripMenuItem.Name = "deleteSelectedToolStripMenuItem";
+            this.deleteSelectedToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.R)));
+            this.deleteSelectedToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.deleteSelectedToolStripMenuItem.Text = "Delete Selected";
+            this.deleteSelectedToolStripMenuItem.Click += new System.EventHandler(this.deleteSelectedToolStripMenuItem_Click);
+            // 
+            // rollbackToolStripMenuItem
+            // 
+            this.rollbackToolStripMenuItem.Name = "rollbackToolStripMenuItem";
+            this.rollbackToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.Z)));
+            this.rollbackToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.rollbackToolStripMenuItem.Text = "Rollback";
+            this.rollbackToolStripMenuItem.Click += new System.EventHandler(this.rollbackToolStripMenuItem_Click);
+            // 
+            // clearAllToolStripMenuItem
+            // 
+            this.clearAllToolStripMenuItem.Name = "clearAllToolStripMenuItem";
+            this.clearAllToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.clearAllToolStripMenuItem.Text = "Clear all";
+            this.clearAllToolStripMenuItem.Click += new System.EventHandler(this.clearAllToolStripMenuItem_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 676);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(84, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Center Point";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // centerPointToolStripMenuItem
+            // 
+            this.centerPointToolStripMenuItem.Name = "centerPointToolStripMenuItem";
+            this.centerPointToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F)));
+            this.centerPointToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.centerPointToolStripMenuItem.Text = "Center Point";
+            this.centerPointToolStripMenuItem.Click += new System.EventHandler(this.centerPointToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1344, 705);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "2DEditor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem pointsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteSelectedToolStripMenuItem;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ToolStripMenuItem rollbackToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem centerPointToolStripMenuItem;
+    }
+}
+
---
+++ 2DEditor/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2DEditor
+{
+    public partial class Form1 : Form
+    {
+        Graphics pbGraph; //рисуем на pb
+        Graphics pbReal;
+        Image bgImg; //фоновая картинка
+        Image ImgLoad; //подргужаемый фон
+        List<Point> Log; //все добавленные и отменённые точки
+        List<Point> ActualPoints; //все активные точки
+        Point MainPoint; //центр вращения
+        Point startRect, startField, endField; //выделение обласи для удаления
+        List<Point> DelPoint; //удаляемые точки
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bgImg = new Bitmap(pictureBox1.Width,pictureBox1.Height);
+            Log = new List<Point>();
+            ActualPoints = new List<Point>();
+            pbGraph = Graphics.FromImage(bgImg);    
+            pbReal = pictureBox1.CreateGraphics();
+            pictureBox1.BackColor = Color.White;
+            MainPoint = new Point();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            if(ActualPoints.Count!=0 && MainPoint!=new Point())
+            {
+                SaveFileDialog sd = new SaveFileDialog();
+                sd.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+                if (sd.ShowDialog() == DialogResult.OK)
+                {
+                    List<float> rad = new List<float>();
+                    List<float> angl = new List<float>();
+                    for (int i = 0; i < ActualPoints.Count; ++i)
+                    {
+                        int first = MainPoint.X - ActualPoints[i].X;
+                        int second = MainPoint.Y - ActualPoints[i].Y;
+                        rad.Add(Convert.ToSingle(Math.Sqrt(Math.Pow((MainPoint.X - ActualPoints[i].X), 2) +
+                            Math.Pow((MainPoint.Y - ActualPoints[i].Y), 2))));
+                        if (ActualPoints[i].Y <= MainPoint.Y) angl.Add(Convert.ToSingle(Math.Acos((ActualPoints[i].X - MainPoint.X) / rad[i]) / Math.PI * 180));
+                        else angl.Add(Convert.ToSingle(360 - Math.Acos((ActualPoints[i].X - MainPoint.X) / rad[i]) / Math.PI * 180));
+                    }
+                    using (StreamWriter sr = new StreamWriter(sd.FileName))
+                    {
+                        sr.WriteLine(MainPoint.X.ToString() + " " + MainPoint.Y.ToString());
+                        for (int i = 0; i < rad.Count; ++i)
+                        {
+                            sr.WriteLine(rad[i].ToString() + " " + angl[i].ToString());
+                        }                        
+                    }
+                }                
+            }
+            else { MessageBox.Show("Empty points or center point"); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            if( od.ShowDialog()== DialogResult.OK)
+            {
+                ImgLoad = new Bitmap(od.FileName);
+                ReDraw();
+            }
+        }
+        
+        bool hasNearPoints(Point check)
+        {
+            for(int i = 0; i < ActualPoints.Count; ++i)            
+                if (ActualPoints[i].X >= check.X - 3 && ActualPoints[i].X <= check.X + 3
+                    && ActualPoints[i].Y >= check.Y - 3 && ActualPoints[i].Y <= check.Y + 3) return true;            
+            return false;
+        }
+        Point GetPoint(Point check)
+        {
+            for (int i = 0; i < ActualPoints.Count; ++i)            
+                if (ActualPoints[i].X >= check.X - 3 && ActualPoints[i].X <= check.X + 3
+                    && ActualPoints[i].Y >= check.Y - 3 && ActualPoints[i].Y <= check.Y + 3) return ActualPoints[i];             
+            return new Point();
+        }
+        
+        void ReDraw()
+        {
+            pbGraph.FillRectangle(Brushes.White, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            if (ImgLoad != null) pbGraph.DrawImage(ImgLoad, 0, 0);
+            if (ImgLoad!=null) pbGraph.DrawImage(ImgLoad, 0, 0);
+            for (int i = 0; i < ActualPoints.Count; ++i)
+            {
+                pbGraph.FillRectangle(Brushes.Red, ActualPoints[i].X - 3, ActualPoints[i].Y - 3, 7, 7);
+                pbGraph.FillRectangle(Brushes.Black, ActualPoints[i].X - 2, ActualPoints[i].Y - 2, 5, 5);
+                pbGraph.FillRectangle(Brushes.Red, ActualPoints[i].X - 1, ActualPoints[i].Y - 1, 3, 3);
+                pbGraph.FillRectangle(Brushes.White, ActualPoints[i].X, ActualPoints[i].Y, 1, 1);
+            }
+            if(MainPoint!= new Point())
+            {
+                pbGraph.FillRectangle(Brushes.Green, MainPoint.X - 4, MainPoint.Y - 4, 9, 9);
+                pbGraph.FillRectangle(Brushes.Green, MainPoint.X - 3, MainPoint.Y - 3, 7, 7);
+                pbGraph.FillRectangle(Brushes.Black, MainPoint.X - 2, MainPoint.Y - 2, 5, 5);
+                pbGraph.FillRectangle(Brushes.Green, MainPoint.X - 1, MainPoint.Y - 1, 3, 3);
+                pbGraph.FillRectangle(Brushes.White, MainPoint.X, MainPoint.Y, 1, 1);
+            }
+            pbReal.DrawImage(bgImg, 0, 0);
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && !hasNearPoints(e.Location))
+            {
+                if(checkBox1.Checked==true)
+                {
+                    MainPoint = new Point(e.X, e.Y);
+                    ReDraw();
+                }
+                else
+                {
+                    Point tmp = new Point(e.X, e.Y);
+                    Log.Add(tmp);
+                    ActualPoints.Add(tmp);
+                    ReDraw();
+                }
+            }
+            else
+            {
+                if( e.Button == MouseButtons.Right && hasNearPoints(e.Location))
+                {
+                    Point tmp = GetPoint(e.Location);
+                    Log.Add(tmp); 
+                    ActualPoints.Remove(tmp);
+                    ReDraw();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Раздел отвечающий за выделение точек правой кнопкой и их удаление
+        /// </summary>
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                timer1.Enabled = true;
+                startRect = e.Location;
+                DelPoint = new List<Point>();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pbGraph.FillRectangle(Brushes.White, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            if (ImgLoad != null) pbGraph.DrawImage(ImgLoad, 0, 0);
+            for (int i = 0; i < ActualPoints.Count; ++i)
+            {
+                pbGraph.FillRectangle(Brushes.Red, ActualPoints[i].X - 3, ActualPoints[i].Y - 3, 7, 7);
+                pbGraph.FillRectangle(Brushes.Black, ActualPoints[i].X - 2, ActualPoints[i].Y - 2, 5, 5);
+                pbGraph.FillRectangle(Brushes.Red, ActualPoints[i].X - 1, ActualPoints[i].Y - 1, 3, 3);
+                pbGraph.FillRectangle(Brushes.White, ActualPoints[i].X, ActualPoints[i].Y, 1, 1);
+            }
+            if (MainPoint != new Point())
+            {
+                pbGraph.FillRectangle(Brushes.Green, MainPoint.X - 4, MainPoint.Y - 4, 9, 9);
+                pbGraph.FillRectangle(Brushes.Green, MainPoint.X - 3, MainPoint.Y - 3, 7, 7);
+                pbGraph.FillRectangle(Brushes.Black, MainPoint.X - 2, MainPoint.Y - 2, 5, 5);
+                pbGraph.FillRectangle(Brushes.Green, MainPoint.X - 1, MainPoint.Y - 1, 3, 3);
+                pbGraph.FillRectangle(Brushes.White, MainPoint.X, MainPoint.Y, 1, 1);
+            }
+            int xLoc = (Cursor.Position.X - this.Location.X - 20) - startRect.X ;
+            int yLoc = (Cursor.Position.Y - this.Location.Y - 57) - startRect.Y ;
+            if (xLoc > 0)
+                if (yLoc > 0) { pbGraph.DrawRectangle(Pens.Black, startRect.X, startRect.Y, xLoc, yLoc); startField = startRect;
+                    endField = new Point(Cursor.Position.X - this.Location.X - 20, Cursor.Position.Y - this.Location.Y - 57);
+                }
+                else { pbGraph.DrawRectangle(Pens.Black, startRect.X, (Cursor.Position.Y - this.Location.Y - 57), xLoc, -yLoc);
+                    startField = new Point(startRect.X, (Cursor.Position.Y - this.Location.Y - 57));
+                    endField = new Point(Cursor.Position.X - this.Location.X - 20,startRect.Y);
+                }
+            else if (yLoc > 0) { pbGraph.DrawRectangle(Pens.Black, (Cursor.Position.X - this.Location.X - 20), startRect.Y, -xLoc, yLoc);
+                startField = new Point(Cursor.Position.X - this.Location.X - 20, startRect.Y);
+                endField = new Point(startRect.X, (Cursor.Position.Y - this.Location.Y - 57));
+            }
+            else { pbGraph.DrawRectangle(Pens.Black, (Cursor.Position.X - this.Location.X - 20),
+                (Cursor.Position.Y - this.Location.Y - 57), -xLoc, -yLoc);
+                startField = new Point(Cursor.Position.X - this.Location.X - 20, Cursor.Position.Y - this.Location.Y - 57);
+                endField = startRect;
+            }
+            pbReal.DrawImage(bgImg, 0, 0);
+        }
+                
+        List<Point> InRect()
+        {
+            List<Point> res = new List<Point>();
+            for (int i = 0; i < ActualPoints.Count; ++i)
+                if (ActualPoints[i].X >= startField.X && ActualPoints[i].X <= endField.X
+                    && ActualPoints[i].Y >= startField.Y && ActualPoints[i].Y <= endField.Y) { res.Add(ActualPoints[i]); }
+            return res;
+        }
+
+        void RefreshPointInRect(List<Point> lst)
+        {
+            DelPoint = lst;
+            for (int i = 0; i < lst.Count; ++i)
+            {
+                pbGraph.FillRectangle(Brushes.Blue, lst[i].X - 3, lst[i].Y - 3, 7, 7);
+                pbGraph.FillRectangle(Brushes.Black, lst[i].X - 2, lst[i].Y - 2, 5, 5);
+                pbGraph.FillRectangle(Brushes.Blue, lst[i].X - 1, lst[i].Y - 1, 3, 3);
+                pbGraph.FillRectangle(Brushes.White, lst[i].X, lst[i].Y, 1, 1);
+            }
+            pbReal.DrawImage(bgImg, 0, 0);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < DelPoint.Count; ++i)
+            {
+                Log.Add(DelPoint[i]);
+                ActualPoints.Remove(DelPoint[i]);
+            }
+            DelPoint = new List<Point>();
+            ReDraw();
+        }
+        
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                bgImg = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                pbGraph = Graphics.FromImage(bgImg);
+                pbReal = pictureBox1.CreateGraphics();
+                pictureBox1.BackColor = Color.White;
+                ReDraw();
+            }
+            catch { }
+        }
+        //???
+        private void rollbackToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Log.Count > 1)
+            {
+                Point tmp = Log.Last();
+                int k = 0;
+                for (int i = Log.Count - 1; i > 0; --i)
+                    if (Log[i] == tmp) k++;
+                if (k % 2 == 0) { ActualPoints.Add(tmp); Log.RemoveAt(Log.Count - 1); }
+                else { ActualPoints.Remove(tmp); Log.RemoveAt(Log.Count - 1); }
+            }
+            else if (Log.Count != 0) { Log = new List<Point>(); ActualPoints = new List<Point>(); }
+            else { Log.Clear(); ActualPoints.Clear(); }
+            ReDraw();
+        }
+
+        private void clearAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Log = new List<Point>();
+            ActualPoints = new List<Point>();
+            ReDraw();
+        }
+
+        private void centerPointToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            checkBox1.Checked = !checkBox1.Checked;
+        }
+
+        private void deleteSelectedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < DelPoint.Count; ++i)
+            {
+                ActualPoints.Remove(DelPoint[i]);
+            }
+            DelPoint = new List<Point>();
+            ReDraw();
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                timer1.Enabled = false;
+                ReDraw();
+                RefreshPointInRect(InRect());
+            }
+        }
+    }
+}
